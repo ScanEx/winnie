1126e2aba512d425a17c0a4960c77f445b1c4d38
zverev <alexander.zverev@gmail.com>
2015-04-09 13:24:18 +0300

    editor: style and markup fixes

--- editor/editor.js
+++ editor/editor.js
@@ -1,8 +1,34 @@
 (function() {
-    cm.define('editor', [], function(cm) {
-        var editor = ace.edit('editor');
+    cm.define('sampleconfig', [], function() {
+        return {
+            map: {
+                center: [55.750303, 37.619934],
+                zoom: 8
+            },
+            gmxMap: {
+                mapID: '37TYY',
+                apiKey: 'W4IH6K7CJ4'
+            },
+            zoomControl: 'leaflet',
+            hideControl: false,
+            centerControl: {
+                color: '#121212'
+            }
+        };
+    });
+
+    cm.define('editor', ['sampleconfig'], function(cm) {
+        var container = $('<div>')
+            .css('position', 'relative')
+            .css('width', '100%')
+            .css('height', '100%');
+
+        $('.editor-codeFrame').append(container);
+
+        var editor = ace.edit(container.get(0));
         editor.setTheme("ace/theme/chrome");
         editor.getSession().setMode("ace/mode/json");
+        editor.setValue(JSON.stringify(cm.get('sampleconfig'), null, '    '));
         return editor;
     });
 
