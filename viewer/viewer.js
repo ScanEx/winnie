a48a8d14dfde6e33fa70cf83334f5da7f2b0cfc1
zverev <alexander.zverev@gmail.com>
2015-04-10 15:27:48 +0300

    viewer: load from permalink

--- viewer/viewer.js
+++ viewer/viewer.js
@@ -1,4 +1,47 @@
 (function() {
+    cm.define('urlManager', [], function(cm) {
+        var parser = document.createElement('a');
+        parser.href = window.location.href;
+
+        var getQueryVariable = function(variable) {
+            var query = parser.search.substring(1);
+            var vars = query.split('&');
+            for (var i = 0; i < vars.length; i++) {
+                var pair = vars[i].split('=');
+                if (decodeURIComponent(pair[0]) == variable) {
+                    return decodeURIComponent(pair[1]);
+                }
+            }
+        };
+
+        return {
+            getParam: getQueryVariable
+        };
+    });
+
+    cm.define('config', ['urlManager'], function(cm, cb) {
+        var urlManager = cm.get('urlManager');
+        if (urlManager.getParam('config')) {
+            var oReq = new XMLHttpRequest();
+            oReq.onload = function(e) {
+                if (e.currentTarget.readyState === 4 && e.currentTarget.status === 200) {
+                    try {
+                        var resp = JSON.parse(e.currentTarget.response.slice(1, -1));
+                        var cfg = JSON.parse(resp.Result);
+                        cb(cfg);
+                    } catch (e) {
+                        console.warn('invalid JSON');
+                        cb({});
+                    }
+                }
+            };
+            oReq.open('get', 'http://maps.kosmosnimki.ru/TinyReference/Get.ashx?id=' + urlManager.getParam('config'), true);
+            oReq.send();
+        } else {
+            return {};
+        }
+    });
+
     cm.define('layoutManager', [], function() {
         var mapEl = L.DomUtil.create('div', 'mapContainer', document.body);
         return {
@@ -7,14 +50,11 @@
             }
         }
     });
-    
-    cm.define('mapApplicationConstructor', ['layoutManager'], function(cm, cb) {
+
+    cm.define('mapApplicationConstructor', ['layoutManager', 'config'], function(cm, cb) {
+        var config = cm.get('config');
         var layoutManager = cm.get('layoutManager');
-        var macm = nsGmx.createMapApplication(layoutManager.getMapContainer(), {
-            gmxMap: {
-                mapID: 'UFKQE'
-            }
-        })
+        var macm = nsGmx.createMapApplication(layoutManager.getMapContainer(), config)
         macm.create().then(function() {
             cb(macm);
         });
