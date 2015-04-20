16919b7f29c024f33c145642d2ca3bbd4b83e202
zverev <alexander.zverev@gmail.com>
2015-04-20 14:38:09 +0300

    editor/refactor[1]: configModel + viewer

--- editor/editor.js
+++ editor/editor.js
@@ -7,8 +7,31 @@
         return nsGmx.Auth.getAuthManager();
     });
 
-    cm.define('sampleconfig', [], function() {
+    cm.define('layoutManager', [], function(cm) {
         return {
+            getContainer: function() {
+                return $('.editor');
+            }
+        }
+    });
+
+    cm.define('configManager', [], function() {
+        var ConfigModel = Backbone.Model.extend({
+            initialize: function(cfg) {
+                this.setConfig(cfg);
+                this.on('change:config', function() {
+                    this.trigger('configchange', this.get('config'));
+                }.bind(this));
+            },
+            setConfig: function(cfg) {
+                this.set('config', cfg);
+            },
+            getConfig: function() {
+                return this.get('config');
+            }
+        });
+
+        return new ConfigModel({
             map: {
                 center: [55.750303, 37.619934],
                 zoom: 8
@@ -22,55 +45,23 @@
             centerControl: {
                 color: '#121212'
             },
-            storytellingWidget: {
+            layersTreeWidget: {
 
             }
-        };
-    });
-
-    cm.define('editor', ['sampleconfig'], function(cm) {
-        var container = $('<div>')
-            .css('position', 'relative')
-            .css('width', '100%')
-            .css('height', '100%');
-
-        $('.editor-codeFrame').append(container);
-
-        var editor = ace.edit(container.get(0));
-        editor.setTheme("ace/theme/chrome");
-        editor.getSession().setMode("ace/mode/json");
-        editor.setValue(JSON.stringify(cm.get('sampleconfig'), null, '    '));
-        editor.selection.clearSelection();
-        return editor;
-    });
-
-    cm.define('toolbar', [], function(cm) {
-        var dropdownMenuWidget = new nsGmx.DropdownMenuWidget({
-            items: [{
-                title: 'Refresh',
-                id: 'btn-refresh'
-            }, {
-                title: 'Save',
-                id: 'btn-save'
-            }]
         });
-        dropdownMenuWidget.appendTo($('.editor-toolbarFrame'));
-        return dropdownMenuWidget;
     });
 
-    cm.define('viewer', ['toolbar', 'editor'], function(cm) {
-        var editor = cm.get('editor');
-        var $container = $('.editor-mapFrame');
+    cm.define('viewer', ['configManager', 'layoutManager'], function(cm) {
+        var configManager = cm.get('configManager');
+        var layoutManager = cm.get('layoutManager');
+
+        var $container = layoutManager.getContainer();
 
         var cm;
         var update = function() {
             try {
-                var cfg = JSON.parse(editor.getValue());
-                $container.empty();
-                var $mapContainer = $('<div>')
-                    .css('position', 'relative')
-                    .css('width', '100%')
-                    .css('height', '100%');
+                var cfg = configManager.getConfig();
+                var $mapContainer = $('<div>');
                 $container.append($mapContainer);
                 cm = nsGmx.createGmxApplication($mapContainer.get(0), cfg);
                 cm.create();
@@ -80,9 +71,10 @@
         };
 
         update();
-        $('#btn-refresh').click(function() {
+        configManager.on('configchange', function(cfg) {
             update();
         });
+
         return {
             update: update,
             getCm: function() {
@@ -91,89 +83,152 @@
         };
     });
 
-    cm.define('saveButton', ['toolbar', 'editor', 'mapsResourceServer'], function(cm) {
-        var jsonIsValid = function() {
-            try {
-                JSON.parse(editor.getValue());
-                return true;
-            } catch (e) {
-                return false;
-            }
-        };
+    // cm.define('editor', ['sampleconfig'], function(cm) {
+    //     var container = $('<div>')
+    //         .css('position', 'relative')
+    //         .css('width', '100%')
+    //         .css('height', '100%');
 
-        var editor = cm.get('editor');
-        var mapsResourceServer = cm.get('mapsResourceServer');
+    //     $('.editor-codeFrame').append(container);
 
-        $('#btn-save').popover({
-            content: '<div id="popover-save"></div>',
-            container: 'body',
-            placement: 'bottom',
-            html: true
-        });
+    //     var editor = ace.edit(container.get(0));
+    //     editor.setTheme("ace/theme/chrome");
+    //     editor.getSession().setMode("ace/mode/json");
+    //     editor.setValue(JSON.stringify(cm.get('sampleconfig'), null, '    '));
+    //     editor.selection.clearSelection();
+    //     return editor;
+    // });
 
-        $('#btn-save').on('shown.bs.popover', function() {
-            var origin = window.location.search ?
-                window.location.href.slice(0, window.location.href.indexOf(window.location.search)) :
-                window.location.href;
+    // cm.define('toolbar', [], function(cm) {
+    //     var dropdownMenuWidget = new nsGmx.DropdownMenuWidget({
+    //         items: [{
+    //             title: 'Refresh',
+    //             id: 'btn-refresh'
+    //         }, {
+    //             title: 'Save',
+    //             id: 'btn-save'
+    //         }]
+    //     });
+    //     dropdownMenuWidget.appendTo($('.editor-toolbarFrame'));
+    //     return dropdownMenuWidget;
+    // });
 
-            $('#popover-save').html(Handlebars.compile(nsGmx.Templates.Editor.saveDialog)({
-                permalink: false
-            }));
+    // cm.define('viewer', ['toolbar', 'editor'], function(cm) {
+    //     var editor = cm.get('editor');
+    //     var $container = $('.editor-mapFrame');
 
-            if (jsonIsValid()) {
-                mapsResourceServer.sendPostRequest('TinyReference/Create.ashx', {
-                    content: editor.getValue()
-                }).then(function(response) {
-                    $('#popover-save').html(Handlebars.compile(nsGmx.Templates.Editor.saveDialog)({
-                        permalink: origin.replace('editor.html', 'viewer.html') + '?config=' + response.Result
-                    }));
-                }).fail(function() {
-                    $('#popover-save').html('unknown error');
-                });
-            } else {
-                $('#popover-save').html('invalid json');
-            }
-        });
+    //     var cm;
+    //     var update = function() {
+    //         try {
+    //             var cfg = JSON.parse(editor.getValue());
+    //             $container.empty();
+    //             var $mapContainer = $('<div>')
+    //                 .css('position', 'relative')
+    //                 .css('width', '100%')
+    //                 .css('height', '100%');
+    //             $container.append($mapContainer);
+    //             cm = nsGmx.createGmxApplication($mapContainer.get(0), cfg);
+    //             cm.create();
+    //         } catch (e) {
+    //             console.log('invalid JSON');
+    //         }
+    //     };
 
-        $('#btn-save').on('hide.bs.popover', function() {
-            $('#popover-save').empty();
-        });
-        return true;
-    });
+    //     update();
+    //     $('#btn-refresh').click(function() {
+    //         update();
+    //     });
+    //     return {
+    //         update: update,
+    //         getCm: function() {
+    //             return cm;
+    //         }
+    //     };
+    // });
 
-    cm.define('collapseButton', ['viewer', 'editor'], function() {
-        var viewer = cm.get('viewer');
-        var editor = cm.get('editor');
-        var $mapView = $('.editor-mapFrame');
-        var $sidebarView = $('.editor-sidebarFrame');
-        var $buttonView = $('.editor-collapseButton');
-        var toggleButtonState = function() {
-            $buttonView.toggleClass('icon-angle-left', !$sidebarView.hasClass('editor_sidebarCollapsed'));
-            $buttonView.toggleClass('icon-angle-right', $sidebarView.hasClass('editor_sidebarCollapsed'));
-        };
-        var kickMap = function() {
-            viewer.getCm().get('map') && viewer.getCm().get('map').invalidateSize();
-            editor.resize();
-        };
-        $sidebarView.on('transitionend', function(je) {
-            if (je.originalEvent.propertyName === 'width') {
-                if (!$sidebarView.hasClass('editor_sidebarCollapsed')) {
-                    $mapView.removeClass('editor_sidebarCollapsed');
-                }
-                kickMap();
-            }
-        });
-        $buttonView.on('click', function() {
-            $sidebarView.toggleClass('editor_sidebarCollapsed');
-            if ($sidebarView.hasClass('editor_sidebarCollapsed')) {
-                $mapView.addClass('editor_sidebarCollapsed');
-            }
-            toggleButtonState();
-            kickMap();
-        });
-        toggleButtonState();
-        return true;
-    });
+    // cm.define('saveButton', ['toolbar', 'editor', 'mapsResourceServer'], function(cm) {
+    //     var jsonIsValid = function() {
+    //         try {
+    //             JSON.parse(editor.getValue());
+    //             return true;
+    //         } catch (e) {
+    //             return false;
+    //         }
+    //     };
+
+    //     var editor = cm.get('editor');
+    //     var mapsResourceServer = cm.get('mapsResourceServer');
+
+    //     $('#btn-save').popover({
+    //         content: '<div id="popover-save"></div>',
+    //         container: 'body',
+    //         placement: 'bottom',
+    //         html: true
+    //     });
+
+    //     $('#btn-save').on('shown.bs.popover', function() {
+    //         var origin = window.location.search ?
+    //             window.location.href.slice(0, window.location.href.indexOf(window.location.search)) :
+    //             window.location.href;
+
+    //         $('#popover-save').html(Handlebars.compile(nsGmx.Templates.Editor.saveDialog)({
+    //             permalink: false
+    //         }));
+
+    //         if (jsonIsValid()) {
+    //             mapsResourceServer.sendPostRequest('TinyReference/Create.ashx', {
+    //                 content: editor.getValue()
+    //             }).then(function(response) {
+    //                 $('#popover-save').html(Handlebars.compile(nsGmx.Templates.Editor.saveDialog)({
+    //                     permalink: origin.replace('editor.html', 'viewer.html') + '?config=' + response.Result
+    //                 }));
+    //             }).fail(function() {
+    //                 $('#popover-save').html('unknown error');
+    //             });
+    //         } else {
+    //             $('#popover-save').html('invalid json');
+    //         }
+    //     });
+
+    //     $('#btn-save').on('hide.bs.popover', function() {
+    //         $('#popover-save').empty();
+    //     });
+    //     return true;
+    // });
+
+    // cm.define('collapseButton', ['viewer', 'editor'], function() {
+    //     var viewer = cm.get('viewer');
+    //     var editor = cm.get('editor');
+    //     var $mapView = $('.editor-mapFrame');
+    //     var $sidebarView = $('.editor-sidebarFrame');
+    //     var $buttonView = $('.editor-collapseButton');
+    //     var toggleButtonState = function() {
+    //         $buttonView.toggleClass('icon-angle-left', !$sidebarView.hasClass('editor_sidebarCollapsed'));
+    //         $buttonView.toggleClass('icon-angle-right', $sidebarView.hasClass('editor_sidebarCollapsed'));
+    //     };
+    //     var kickMap = function() {
+    //         viewer.getCm().get('map') && viewer.getCm().get('map').invalidateSize();
+    //         editor.resize();
+    //     };
+    //     $sidebarView.on('transitionend', function(je) {
+    //         if (je.originalEvent.propertyName === 'width') {
+    //             if (!$sidebarView.hasClass('editor_sidebarCollapsed')) {
+    //                 $mapView.removeClass('editor_sidebarCollapsed');
+    //             }
+    //             kickMap();
+    //         }
+    //     });
+    //     $buttonView.on('click', function() {
+    //         $sidebarView.toggleClass('editor_sidebarCollapsed');
+    //         if ($sidebarView.hasClass('editor_sidebarCollapsed')) {
+    //             $mapView.addClass('editor_sidebarCollapsed');
+    //         }
+    //         toggleButtonState();
+    //         kickMap();
+    //     });
+    //     toggleButtonState();
+    //     return true;
+    // });
 
     cm.create();
 })();
