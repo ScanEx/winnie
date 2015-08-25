c2455d3208de63f1b0c1663e29dce779d03720b3
zverev <alexander.zverev@gmail.com>
2015-08-25 15:33:41 +0300

    use 'app/state' config format

--- editor/configTemplates.js
+++ editor/configTemplates.js
@@ -3,50 +3,74 @@ var nsGmx = nsGmx || {};
 nsGmx.ConfigTemplates = {};
 
 nsGmx.ConfigTemplates.map = {
-    map: {
-        center: [55.750303, 37.619934],
-        zoom: 8
+    app: {
+        gmxMap: {
+            mapID: '37TYY',
+            apiKey: 'W4IH6K7CJ4'
+        },
+        zoomControl: true,
+        hideControl: true,
+        centerControl: {
+            color: '#121212'
+        }
     },
-    gmxMap: {
-        mapID: '37TYY',
-        apiKey: 'W4IH6K7CJ4'
-    },
-    zoomControl: true,
-    hideControl: true,
-    centerControl: {
-        color: '#121212'
+    state: {
+        map: {
+            position: {
+                x: 37.619934,
+                y: 55.750303,
+                z: 8
+            }
+        },
+        language: 'rus'
     }
 };
 
 nsGmx.ConfigTemplates.sidebar = {
-    map: {
-        center: [55.750303, 37.619934],
-        zoom: 8
+    app: {
+        gmxMap: {
+            mapID: '37TYY',
+            apiKey: 'W4IH6K7CJ4'
+        },
+        zoomControl: true,
+        hideControl: true,
+        centerControl: {
+            color: '#121212'
+        },
+        layersTreeWidget: {}
     },
-    gmxMap: {
-        mapID: '37TYY',
-        apiKey: 'W4IH6K7CJ4'
-    },
-    zoomControl: true,
-    hideControl: true,
-    centerControl: {
-        color: '#121212'
-    },
-    layersTreeWidget: {}
+    state: {
+        map: {
+            position: {
+                x: 37.619934,
+                y: 55.750303,
+                z: 8
+            }
+        },
+        language: 'rus'
+    }
 };
 
 nsGmx.ConfigTemplates.storytelling = {
-    map: {
-        center: [55.750303, 37.619934],
-        zoom: 8
+    app: {
+        gmxMap: {
+            mapID: '37TYY',
+            apiKey: 'W4IH6K7CJ4'
+        },
+        zoomControl: false,
+        hideControl: false,
+        centerControl: false,
+        baseLayersControl: false,
+        storytellingWidget: {}
     },
-    gmxMap: {
-        mapID: '37TYY',
-        apiKey: 'W4IH6K7CJ4'
-    },
-    zoomControl: false,
-    hideControl: false,
-    centerControl: false,
-    baseLayersControl: false,
-    storytellingWidget: {}
+    state: {
+        map: {
+            position: {
+                x: 37.619934,
+                y: 55.750303,
+                z: 8
+            }
+        },
+        language: 'rus'
+    }
 };
